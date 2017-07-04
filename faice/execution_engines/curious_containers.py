ff32d2402a3848a7c38c6dc3f1c730f2594d610c
Christoph Jansen <christoph@gnork.org>
2017-07-04 23:39:34 +0200

    improved vagrant in curious-containers engine

--- faice/execution_engines/curious_containers.py
+++ faice/execution_engines/curious_containers.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 import jsonschema
 from copy import deepcopy
@@ -66,17 +67,8 @@ def run(d):
     pprint(r.json())
 
 
-def adapt(d, use_local_execution_engine=False, use_local_input_files=False, use_local_result_files=False):
+def adapt(d, use_local_input_files=False, use_local_result_files=False):
     c = deepcopy(d)
-
-    if use_local_execution_engine:
-        c['experiment']['execution_engine']['engine_config'] = {
-            'url': 'http://localhost:8000',
-            'auth': {
-                'username': 'user',
-                'password': 'PASSWORD'
-            }
-        }
 
     if use_local_input_files:
         if c['experiment']['instructions'].get('tasks'):
@@ -114,6 +106,20 @@ def adapt(d, use_local_execution_engine=False, use_local_input_files=False, use_
     return c
 
 
+def _adapt_for_vagrant(d, port):
+    c = deepcopy(d)
+
+    c['experiment']['execution_engine']['engine_config'] = {
+        'url': 'http://localhost:{}'.format(port),
+        'auth': {
+            'username': 'user',
+            'password': 'PASSWORD'
+        }
+    }
+
+    return c
+
+
 def vagrant(d, output_directory):
     engine_config = d['experiment']['execution_engine']['engine_config']
 
@@ -125,12 +131,13 @@ def vagrant(d, output_directory):
     vm_memory = 4096
     vm_cpus = 2
     vm_box = 'trusty64'
-    vm_box_url = 'https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-vagrant.box',
+    vm_box_url = 'https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-vagrant.box'
     vm_user = 'ubuntu'
 
     vagrant_file_name = 'Vagrantfile'
     provision_file_name = 'provision.sh'
     compose_file_name = 'docker-compose.yml'
+    experiment_file_name = 'experiment.json'
 
     vagrant_file_lines = [
         'VAGRANTFILE_API_VERSION = "2"',
@@ -165,9 +172,17 @@ def vagrant(d, output_directory):
         '> /usr/local/bin/docker-compose'.format(docker_compose_version),
         'chmod +x /usr/local/bin/docker-compose',
         '',
-        'su {}'.format(vm_user),
         'cd ~',
         'git clone -b {} --depth 1 https://github.com/curious-containers/cc-server.git'.format(cc_server_version),
+        '',
+        'cd ~/cc-server',
+        'cp /vagrant/{} ./compose'.format(compose_file_name),
+        'cp compose/config_samples/config.toml compose',
+        'cp compose/config_samples/credentials.toml compose',
+        'bash compose/scripts/create_systemd_unit_file -d $(pwd)',
+        '',
+        'systemctl enable cc-server',
+        'systemctl start cc-server',
         ''
     ]
 
@@ -188,6 +203,77 @@ def vagrant(d, output_directory):
         '      - cc-server-master',
         '      - cc-server-log',
         '    tty: true',
+        '',
+        '  cc-server-master:',
+        '    build: ./cc-server-image',
+        '    command: "python3 -u /root/.config/curious-containers/cc-server-master/init.py"',
+        '    volumes:',
+        '      - ../cc_server_master:/opt/cc_server_master:ro',
+        '      - ../cc_commons:/opt/cc_commons:ro',
+        '      - .:/root/.config/curious-containers:ro',
+        '    links:',
+        '      - mongo',
+        '      - dind',
+        '      - cc-server-log',
+        '    depends_on:',
+        '      - mongo-seed',
+        '    tty: true',
+        '',
+        '  cc-server-log:',
+        '    build: ./cc-server-image',
+        '    command: "python3 -u /opt/cc_server_log"',
+        '    volumes:',
+        '      - ../cc_server_log:/opt/cc_server_log:ro',
+        '      - ../cc_commons:/opt/cc_commons:ro',
+        '      - .:/root/.config/curious-containers:ro',
+        '      - /vagrant/curious-containers/logs:/root/.cc_server/logs',
+        '    tty: true',
+        '',
+        '  mongo:',
+        '    image: mongo',
+        '    ports:',
+        '      - "27017:27017"',
+        '    volumes:',
+        '      - /vagrant/curious-containers/mongo/db:/data/db',
+        '    tty: true',
+        '',
+        '  mongo-seed:',
+        '    build: ./mongo-seed',
+        '    volumes:',
+        '      - .:/root/.config/curious-containers',
+        '    command: "python3 -u /root/.config/curious-containers/mongo-seed/init.py"',
+        '    links:',
+        '      - mongo',
+        '    tty: true',
+        '',
+        '  dind:',
+        '    image: docker:dind',
+        '    privileged: true',
+        '    command: "docker daemon --insecure-registry=registry:5000 -H tcp://0.0.0.0:2375"',
+        '    volumes:',
+        '      - /vagrant/curious-containers/dind/docker:/var/lib/docker',
+        '    links:',
+        '      - registry',
+        '      - file-server',
+        '    tty: true',
+        '',
+        '  file-server:',
+        '    build: ./cc-server-image',
+        '    command: "gunicorn -w 4 -b 0.0.0.0:80 file_server.__main__:app"',
+        '    volumes:',
+        '      - ./file_server:/opt/file_server:ro',
+        '      - /vagrant/curious-containers/input_files:/root/input_files:ro',
+        '      - /vagrant/curious-containers/result_files:/root/result_files',
+        '    tty: true',
+        '',
+        '  registry:',
+        '    image: registry:2',
+        '    ports:',
+        '      - "5000:5000"',
+        '    environment:',
+        '      REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY: /data',
+        '    volumes:',
+        '    - /vagrant/curious-containers/registry/data:/data',
         ''
     ]
 
@@ -202,3 +288,7 @@ def vagrant(d, output_directory):
         file_path = os.path.join(output_directory, file_name)
         with open(file_path, 'w') as f:
             f.write(file_content)
+
+    c = _adapt_for_vagrant(d, port=cc_host_port)
+    with open(os.path.join(output_directory, experiment_file_name), 'w') as f:
+        json.dump(c, f, indent=4)
