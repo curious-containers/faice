74c53cc48be4e2001e973122a0b71205afdf2503
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2017-08-07 11:17:32 +0200

    improved README

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='faice',
-    version='0.11',
+    version='1.0',
     summary='FAICE (Fair Collaboration and Experiments) is a tool suite, helping researchers to work with experiments '
             'published in the FAICE description format.',
     description='FAICE (Fair Collaboration and Experiments) is a tool suite, helping researchers to work with '
